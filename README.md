85a386c50f3f69c9c878db6c8f5e1745853b94ab
MaxwellM34 <maxwellmcinnis123@gmail.com>
2026-04-05 14:45:12 -0400

    add argsparser for prompt input and message tracking / chat history

---
+++ README.md
@@ -0,0 +1,5 @@
+# Agent 
+
+Agent is a simple boot.dev course that goes over Gemini API usage.
+
+Need UV and python, review .toml.
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
+from google.genai import types
+
 
 
 def main():
@@ -13,14 +16,25 @@ def main():
         print("API Key loaded successfully.")
     else:
         raise RuntimeError("API Key not found. Please set GEMINI_API_KEY in your .env file.")
-    
+        
     client = genai.Client(api_key=api_key)
-    
+    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    # Now we can access `args.user_prompt`
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        model='gemini-2.5-flash', contents=messages
     )
-    print(response.text)
 
-
+    if response.usage_metadata:
+        print(f"User prompt: {args.user_prompt}\n")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+        print(f"Response:\n{response.text}")
+    else:
+        raise RuntimeError("Response was not generated")
+        
 if __name__ == "__main__":
     main()
