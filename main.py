5c42604ed505fda6bc436f986f899f4cd753a285
MaxwellM34 <maxwellmcinnis123@gmail.com>
2026-04-05 15:02:32 -0400

    add optional verbose output, minimize base output

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def main():
     prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
@@ -30,9 +31,10 @@ def main():
     )
 
     if response.usage_metadata:
-        print(f"User prompt: {args.user_prompt}\n")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
         print(f"Response:\n{response.text}")
+        if args.verbose == True:
+            print(f"User prompt: {args.user_prompt}\n")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
     else:
         raise RuntimeError("Response was not generated")
         
