9219c077cf5f07a29eea18b562eea4aded1d2837
MaxwellM34 <maxwellmcinnis123@gmail.com>
2026-04-27 15:28:50 -0400

    followign intstructions

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,15 @@
-def get_files_info(working_directory, directory=".:
-    os.path.abspath()
+import os
+
+def get_files_info(working_directory, directory="~/workspace/bootdev/agent"):
+    working_dir_abs = os.path.abspath(directory)
+    print(working_dir_abs)
+    print(f"hello{os.path.abspath(directory)}")
     target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
     os.path.commonpath()
+
+
+def main():
+    get_files_info("agent")
+
+if __name__ == "__main__":
+    main()
